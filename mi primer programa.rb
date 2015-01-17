7c79e43fdf8eec6ce55ad991a360f63b23722e9d
Dani Potter <DANIPOTTER@DANIELs-MacBook-Pro.local>
2015-01-16 21:27:25 -0500

    elprimercambio

---
+++ mi primer programa.rb
@@ -0,0 +1,12 @@
+miprimerprograma.rb
+
+
+
+puts"Hola Mundo"
+tu me gustas =  true
+yo te gusto = true
+if true 
+	puts "quieres ser mi pareja"
+else "entonces podemos ser amigos"
+end
+
---
+++ miprimerprograma.rb
@@ -0,0 +1,12 @@
+
+
+
+
+puts "Hola Mundo"
+tu_me_gustas = true
+yo_te_gusto = true
+if true
+	puts "quieres ser mi pareja"
+else "entonces podemos ser amigos"
+end
+
